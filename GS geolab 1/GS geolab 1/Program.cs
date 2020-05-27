fe897b55a27528c827e008014f240d76f064a7d5
granlite <65130690+granlite@users.noreply.github.com>
2020-05-27 22:08:58 +0400

    Add files via upload

---
+++ GS geolab 1/GS geolab 1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GS_geolab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("write the number you want to find: ");
+            string pretarget = Console.ReadLine();
+            int target = Int32.Parse(pretarget);
+            List<int> list = new List<int>() {2,4,5,7};
+            int i = 0;
+            while (i < list.Count)
+            {
+                int a = list.ElementAt(i);
+                if (a == target)
+                {
+                    Console.WriteLine("true " + list.IndexOf(a));
+                    i = list.Count;
+                }
+                else
+                {
+                    Console.WriteLine("false " + list.IndexOf(a));
+                }
+                i++;
+            }
+        }
+    }
+}
